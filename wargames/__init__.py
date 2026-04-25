a7e96ea7fd70dc747b1dbc8e2349c9a966671007
Aaron Kazah <aaronkazah@gmail.com>
2026-04-25 16:39:07 +0100

    Expose Prime environment from root package
    Co-authored-by: Brain <brain@layerbrain.com>

--- wargames/__init__.py
+++ wargames/__init__.py
@@ -1,4 +1,10 @@
 from wargames.core.config import WarGamesConfig
 from wargames.core.runtime.arena import GameDescriptor, WarGames
 
-__all__ = ["GameDescriptor", "WarGames", "WarGamesConfig"]
+def load_environment(*args: object, **kwargs: object) -> object:
+    from wargames.environments.prime import load_environment as load_prime_environment
+
+    return load_prime_environment(*args, **kwargs)
+
+
+__all__ = ["GameDescriptor", "WarGames", "WarGamesConfig", "load_environment"]
